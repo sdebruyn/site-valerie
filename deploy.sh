f862d794842e8972cbbe628f5f70def213540787
Sam Debruyn <sam@debruyn.dev>
2025-10-18 22:00:47 +0200

    remove swetrix

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,4 @@
 set -ex
 
 hugo
-curl -sLo public/t1.js "https://cdn.jsdelivr.net/gh/Swetrix/swetrix-js@latest/dist/swetrix.js"
-curl -sLo public/t1.js.map "https://cdn.jsdelivr.net/gh/Swetrix/swetrix-js@latest/dist/swetrix.js.map"
-curl -sLo public/t2.js "https://umami.debruyn.dev/script.js"
+curl -sLo public/t.js "https://umami.debruyn.dev/script.js"
